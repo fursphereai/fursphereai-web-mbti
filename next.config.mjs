13eb35ecde3980705e9765e0977cc44de5ff9bb4
Keyi Li <keyili@Keyis-Laptop.lan>
2024-06-28 10:16:32 -0700

    Initial commit

--- next.config.mjs
+++ next.config.mjs
@@ -1,6 +1,13 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-
+  async rewrites() {
+    return [
+      {
+        source: '/api/proxy/:path*',
+        destination: 'https://cdn.discordapp.com/:path*',
+      },
+    ];
+  },
   };
   
   export default nextConfig;
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,28 +1,46 @@
-import type { NextPage } from 'next';
-import Image from 'next/image';
+"use client";
 
-const Home: NextPage = () => {
+import React,{ useEffect, useState } from 'react';
+import Image from 'next/image';
+import styles from './Home.module.css'; 
+
+const Home = () => {
+  const [avatarUrl, setAvatarUrl] = useState('');
+
+  useEffect(() => {
+    const avatarPath = 'widget-avatars/rJ5W3JR1ALahUS4QSt7MhVatf89Vn9LSSY1dUzaRQQ8/lURE0N8GGwPSIoS_wZ4AwK99qHJFsa7PVsTTJ6TUkgmhwVn7d5HLVbC3yw8Ts-xHjwWkUelmtOzVvqWJbw0';
+    setAvatarUrl(`/api/proxy/${encodeURIComponent(avatarPath)}`);
+  }, []);
+
   return (
-    <div className="min-h-screen bg-gradient-to-r from-blue-100 to-purple-100">
-      <header className="bg-transparent p-4">
-        <div className="container mx-auto flex items-center">
-          <Image src="/logo.png" alt="Logo" width={50} height={50} />
+    <div className="min-h-screen bg-gradient-to-r from-blue-100 to-purple-100 relative">
+      <header className="bg-transparent p-4 absolute top-0 left-0">
+        <div className="flex items-center">
+          <Image src="/logo.png" alt="Logo" width={50} height={50} priority />
         </div>
       </header>
-      <main className="container mx-auto flex items-center justify-center" style={{ height: 'calc(100vh - 80px)' }}>
-        <div className="w-full max-w-4xl p-4 bg-white rounded-lg shadow-lg">
-        <iframe 
-        src="https://discord.com/widget?id=1252434184434221076&theme=dark" 
-        width="350" 
-        height="500"
-        style={{ border: 0 }} 
-        sandbox="allow-popups allow-popups-to-escape-sandbox allow-same-origin allow-scripts allow-forms"
-        ></iframe>
+      <main className="flex items-center justify-center min-h-screen flex-col ">
+      <div className="flex flex-col items-center mb-12 relative">
+          <span className={styles.text}>Join our Discord！</span>
+          <a href="https://discord.gg/pa2B2yre" target="_blank" rel="noopener noreferrer" className="flex items-center mt-4">
+            <span className={styles.clickText}>Click here!</span>
+            <svg className={styles.arrow} viewBox="0 0 200 50">
+              <path d="M0,25 L175,25" stroke="white" strokeWidth="5" fill="none" markerEnd="url(#arrowhead)" />
+              <defs>
+                <marker id="arrowhead" markerWidth="10" markerHeight="7" refX="10" refY="3.5" orient="auto">
+                  <polygon points="0 0, 10 3.5, 0 7" fill="white" />
+                </marker>
+              </defs>
+            </svg>
+            <Image src="/discord-logo.png" alt="Join our Discord" width={150} height={150} />
+          </a>
+        </div>
+        <div className="flex flex-col items-center">
+          <Image src="/discord.png" alt="Screenshot" width={850} height={420} />
         </div>
       </main>
     </div>
   );
 };
-
 
 export default Home;
