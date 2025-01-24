7702231276397967f0cfc3e73eb453a8dca6de91
Keyi Li <keyili@Keyis-Laptop.local>
2025-01-24 01:08:13 -0600

    message

--- next.config.mjs
+++ next.config.mjs
@@ -1,5 +1,9 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
+  eslint: {
+    // 在构建过程中忽略 ESLint 错误
+    ignoreDuringBuilds: true,
+  },
   async rewrites() {
     return [
       {
