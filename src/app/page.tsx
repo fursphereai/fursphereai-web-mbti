c65ec6406be536abe6df56ac31679a750e2138d7
keyi li <keyili@keyis-MacBook-Pro.local>
2025-02-09 22:22:56 -0600

    Keyi's Change

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,7 +2,7 @@
 
 import React,{ useEffect, useRef, useState } from 'react';
 import Header from './components/Header';
-import Section1 from './components/section1';
+import Section1 from './components/Section1';
 import Section2 from './components/Section2';
 import Section3 from './components/Section3';
 import Section4 from './components/Section4';
