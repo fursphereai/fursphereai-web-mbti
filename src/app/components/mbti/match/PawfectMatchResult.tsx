00e149e8ebc8f378fc67ded3846b79a8ccce1ff0
keyi li <keyili@keyis-MacBook-Pro.local>
2025-02-19 02:28:15 -0600

    Jaaon's update

--- src/app/components/mbti/match/PawfectMatchResult.tsx
+++ src/app/components/mbti/match/PawfectMatchResult.tsx
@@ -1,70 +1,72 @@
-import { useState } from "react";
+// import { useState } from "react";
 
-interface SurveyData {
-  user_info: {
-    email: string,
-    ip: string;
-    name: string,
-    mbti: string
-  };
-  pet_info: {
-    PetKind: string;
-    PetName: string,
-    PetBreed: string,
-    PetSex: string,
-    PetAge: string
-  };
-  personality_and_behavior: {
-      Energy_Socialization: {
-          seek_attention: string,
-          react_new_people: string,
-          behave_with_animals: string,
-      },
-      Routin_Curiosity: {
-          prefer_routine: string,
-          react_new_env: string,
-          lost_in_thought: string,
-      },
-      Decision_Making: {
-          react_when_sad: string,
-          face_challenge: string,
-          hold_grudges:string,
-      },
-      Structure_Spontaneity: {
-          prefer_structure:string,
-          react_unexpected_change:string,
-          follow_commands:string,
-      };
-  };
-}
+// interface SurveyData {
+//   user_info: {
+//     email: string,
+//     ip: string;
+//     name: string,
+//     mbti: string
+//   };
+//   pet_info: {
+//     PetKind: string;
+//     PetName: string,
+//     PetBreed: string,
+//     PetSex: string,
+//     PetAge: string
+//   };
+//   personality_and_behavior: {
+//       Energy_Socialization: {
+//           seek_attention: string,
+//           react_new_people: string,
+//           behave_with_animals: string,
+//       },
+//       Routin_Curiosity: {
+//           prefer_routine: string,
+//           react_new_env: string,
+//           lost_in_thought: string,
+//       },
+//       Decision_Making: {
+//           react_when_sad: string,
+//           face_challenge: string,
+//           hold_grudges:string,
+//       },
+//       Structure_Spontaneity: {
+//           prefer_structure:string,
+//           react_unexpected_change:string,
+//           follow_commands:string,
+//       };
+//   };
+// }
 
-interface PawfectMatchResultProps {
-  handleNext: () => void; 
-  surveyData: SurveyData;
-  updateAnswer: (category: keyof SurveyData, subCategory:any  | null, field: string, value: string) => void;
-}
+// interface PawfectMatchResultProps {
+//   handleNext: () => void; 
+//   surveyData: SurveyData;
+//   step: number;
+//   setStep: React.Dispatch<React.SetStateAction<number>>;
+//   updateAnswer: (category: keyof SurveyData, subCategory:any  | null, field: string, value: string) => void;
+// }
 
-const PawfectMatchResult: React.FC<PawfectMatchResultProps>  = ({ handleNext,step, setStep,  surveyData, updateAnswer  }) => {
+// const PawfectMatchResult: React.FC<PawfectMatchResultProps>  = ({ handleNext,step, setStep,  surveyData, updateAnswer  }) => {
 
 
-  return (
-    <div className="p-4 bg-gray-200 rounded-lg shadow-lg max-w-md mx-auto">
-      <div className="bg-blue-600 text-white text-xl font-bold p-3 rounded-t-lg text-center">
-       Pawfect Match Result
-      </div>
-      <div className="bg-gray-100 p-6 rounded-b-lg text-center">
-       <div className=" text-black text-[50px] font-bold p-3 rounded-t-lg text-center">
-       Pawfect Match Result
-      </div>
-        <button 
-          className="mt-6 px-4 py-2 bg-blue-500 text-white rounded-lg"
-          onClick={handleNext}
-        >
-          Done
-        </button>
-      </div>
-    </div>
-  );
-};
+//   return (
+//     <div className="p-4 bg-gray-200 rounded-lg shadow-lg max-w-md mx-auto">
+//       <div className="bg-blue-600 text-white text-xl font-bold p-3 rounded-t-lg text-center">
+//        Pawfect Match Result
+//       </div>
+//       <div className="bg-gray-100 p-6 rounded-b-lg text-center">
+//        <div className=" text-black text-[50px] font-bold p-3 rounded-t-lg text-center">
+//        Pawfect Match Result
+//       </div>
+//         <button 
+//           className="mt-6 px-4 py-2 bg-blue-500 text-white rounded-lg"
+//           onClick={handleNext}
+//         >
+//           Done
+//         </button>
+//       </div>
+//     </div>
+//   );
+// };
 
-export default PawfectMatchResult;
+// export default PawfectMatchResult;
--- src/app/mbti/page.tsx
+++ src/app/mbti/page.tsx
@@ -1,199 +1,199 @@
-"use client";
+// "use client";
 
 
-import React, { useEffect, useState } from 'react';
-import StartScreen from '../components/mbti/StartScreen';
-import StartScreen2 from '../components/mbti/StartScreen2';
-import StartScreen3 from '../components/mbti/StartScreen3';
-import SurveyScreen1 from '../components/mbti/test/SurveyScreen1';
-import SurveyScreen2 from '../components/mbti/test/SurveyScreen2';
-import SurveyScreen3 from '../components/mbti/test/SurveyScreen3';
-import SurveyScreen4 from '../components/mbti/test/SurveyScreen4';
-import SurveyScreen5 from '../components/mbti/test/SurveyScreen5';
-import EmailVerificationScreen from '../components/mbti/email/EmailVerificationScreen'
-import MbtiResult from '../components/mbti/result/MbtiResult';
-import StartPawfectMatch from '../components/mbti/match/StartPawfectMatch';
-import PawfectMatch from '../components/mbti/match/PawfectMatch';
-import PawfectMatchResult from '../components/mbti/match/PawfectMatchResult';
+// import React, { useEffect, useState } from 'react';
+// import StartScreen from '../components/mbti/StartScreen';
+// import StartScreen2 from '../components/mbti/StartScreen2';
+// import StartScreen3 from '../components/mbti/StartScreen3';
+// import SurveyScreen1 from '../components/mbti/test/SurveyScreen1';
+// import SurveyScreen2 from '../components/mbti/test/SurveyScreen2';
+// import SurveyScreen3 from '../components/mbti/test/SurveyScreen3';
+// import SurveyScreen4 from '../components/mbti/test/SurveyScreen4';
+// import SurveyScreen5 from '../components/mbti/test/SurveyScreen5';
+// import EmailVerificationScreen from '../components/mbti/email/EmailVerificationScreen'
+// import MbtiResult from '../components/mbti/result/MbtiResult';
+// import StartPawfectMatch from '../components/mbti/match/StartPawfectMatch';
+// import PawfectMatch from '../components/mbti/match/PawfectMatch';
+// import PawfectMatchResult from '../components/mbti/match/PawfectMatchResult';
 
-// import TestScreen from './TestScreen';
+// // import TestScreen from './TestScreen';
 
-// import ResultScreen from './ResultScreen';
-// import PawfectMatchScreen from './PawfectMatchScreen';
+// // import ResultScreen from './ResultScreen';
+// // import PawfectMatchScreen from './PawfectMatchScreen';
 
-type SurveyData = {
-  user_info: {
-    email: string,
-    ip: string,
-    name: string,
-    mbti: string
-  };
-  pet_info: {
-    PetKind: string;
-    PetName: string,
-    PetBreed: string,
-    PetSex: string,
-    PetAge: string
-  };
-  personality_and_behavior: {
-      Energy_Socialization: {
-          seek_attention: string,
-          react_new_people: string,
-          behave_with_animals: string,
-      },
-      Routin_Curiosity: {
-          prefer_routine: string,
-          react_new_env: string,
-          lost_in_thought: string,
-      },
-      Decision_Making: {
-          react_when_sad: string,
-          face_challenge: string,
-          hold_grudges:string,
-      },
-      Structure_Spontaneity: {
-          prefer_structure:string,
-          react_unexpected_change:string,
-          follow_commands:string,
-      };
-  };
-};
+// type SurveyData = {
+//   user_info: {
+//     email: string,
+//     ip: string,
+//     name: string,
+//     mbti: string
+//   };
+//   pet_info: {
+//     PetKind: string;
+//     PetName: string,
+//     PetBreed: string,
+//     PetSex: string,
+//     PetAge: string
+//   };
+//   personality_and_behavior: {
+//       Energy_Socialization: {
+//           seek_attention: string,
+//           react_new_people: string,
+//           behave_with_animals: string,
+//       },
+//       Routin_Curiosity: {
+//           prefer_routine: string,
+//           react_new_env: string,
+//           lost_in_thought: string,
+//       },
+//       Decision_Making: {
+//           react_when_sad: string,
+//           face_challenge: string,
+//           hold_grudges:string,
+//       },
+//       Structure_Spontaneity: {
+//           prefer_structure:string,
+//           react_unexpected_change:string,
+//           follow_commands:string,
+//       };
+//   };
+// };
 
-const PetMBTIFlow = () => {
-  const initialStep = 0; 
-  const [step, setStep] = useState(initialStep);
+// const PetMBTIFlow = () => {
+//   const initialStep = 0; 
+//   const [step, setStep] = useState(initialStep);
 
-  // 从 localStorage 中读取步骤
-    // const savedStep = localStorage.getItem('currentStep');
-    // return savedStep ? parseInt(savedStep, 10) : initialStep;
+//   // 从 localStorage 中读取步骤
+//     // const savedStep = localStorage.getItem('currentStep');
+//     // return savedStep ? parseInt(savedStep, 10) : initialStep;
 
-  useEffect(() => {
+//   useEffect(() => {
     
-    const handlePopState = (event: PopStateEvent) => {
-      if (event.state?.step !== undefined) {
-        setStep(event.state.step);
-      } else {
-        setStep((prev) => Math.max(prev - 1, 0));
-      }
-    };
+//     const handlePopState = (event: PopStateEvent) => {
+//       if (event.state?.step !== undefined) {
+//         setStep(event.state.step);
+//       } else {
+//         setStep((prev) => Math.max(prev - 1, 0));
+//       }
+//     };
 
-    window.addEventListener('popstate', handlePopState);
-    return () => window.removeEventListener('popstate', handlePopState);
-  }, []);
+//     window.addEventListener('popstate', handlePopState);
+//     return () => window.removeEventListener('popstate', handlePopState);
+//   }, []);
 
-  useEffect(() => {
-    // step 改变时，更新 URL 和 localStorage
-    window.history.replaceState({ step }, '', `?step=${step}`);
-    localStorage.setItem('currentStep', step.toString());
-  }, [step]);
+//   useEffect(() => {
+//     // step 改变时，更新 URL 和 localStorage
+//     window.history.replaceState({ step }, '', `?step=${step}`);
+//     localStorage.setItem('currentStep', step.toString());
+//   }, [step]);
 
-  const [email, setEmail] = useState('');
-  const [mbtiResult, setMbtiResult] = useState(null);
-  const [isRegistered, setIsRegistered] = useState(false);
+//   const [email, setEmail] = useState('');
+//   const [mbtiResult, setMbtiResult] = useState(null);
+//   const [isRegistered, setIsRegistered] = useState(false);
 
-  const [surveyData, setSurveyData] = useState<SurveyData>({
-    user_info: {
-      email:'',
-      ip: '',
-      name: '',
-      mbti: ''
-    },
-    pet_info: {
-      PetKind: '',
-      PetName: '',
-      PetBreed: '',
-      PetSex: '',
-      PetAge: ''
-    },
-    personality_and_behavior: {
-      Energy_Socialization: {
-        seek_attention: '',
-        react_new_people: '',
-        behave_with_animals: '',
-      },
-      Routin_Curiosity: {
-        prefer_routine: '',
-        react_new_env: '',
-        lost_in_thought: '',
-      },
-      Decision_Making: {
-        react_when_sad: '',
-        face_challenge: '',
-        hold_grudges: '',
-      },
-      Structure_Spontaneity: {
-        prefer_structure: '',
-        react_unexpected_change: '',
-        follow_commands: '',
-      }
-    }
-  });
+//   const [surveyData, setSurveyData] = useState<SurveyData>({
+//     user_info: {
+//       email:'',
+//       ip: '',
+//       name: '',
+//       mbti: ''
+//     },
+//     pet_info: {
+//       PetKind: '',
+//       PetName: '',
+//       PetBreed: '',
+//       PetSex: '',
+//       PetAge: ''
+//     },
+//     personality_and_behavior: {
+//       Energy_Socialization: {
+//         seek_attention: '',
+//         react_new_people: '',
+//         behave_with_animals: '',
+//       },
+//       Routin_Curiosity: {
+//         prefer_routine: '',
+//         react_new_env: '',
+//         lost_in_thought: '',
+//       },
+//       Decision_Making: {
+//         react_when_sad: '',
+//         face_challenge: '',
+//         hold_grudges: '',
+//       },
+//       Structure_Spontaneity: {
+//         prefer_structure: '',
+//         react_unexpected_change: '',
+//         follow_commands: '',
+//       }
+//     }
+//   });
 
 
-  const updateAnswer = <
-  T extends keyof typeof surveyData,
-  K extends keyof typeof surveyData[T]
-    >(
-      category: T,
-      subCategory: K | null,
-      field: keyof typeof surveyData[T] | string,
-      value: string
-    ) => {
-      setSurveyData(prev => ({
-        ...prev,
-        [category]: subCategory
-          ? {
-              ...prev[category],
-              [subCategory]: {
-                ...prev[category][subCategory as keyof typeof surveyData[T]],
-                [field]: value
-              }
-            }
-          : {
-              ...prev[category],
-              [field]: value
-            }
-      }));
-      console.log(surveyData);
-};
+//   const updateAnswer = <
+//   T extends keyof typeof surveyData,
+//   K extends keyof typeof surveyData[T]
+//     >(
+//       category: T,
+//       subCategory: K | null,
+//       field: keyof typeof surveyData[T] | string,
+//       value: string
+//     ) => {
+//       setSurveyData(prev => ({
+//         ...prev,
+//         [category]: subCategory
+//           ? {
+//               ...prev[category],
+//               [subCategory]: {
+//                 ...prev[category][subCategory as keyof typeof surveyData[T]],
+//                 [field]: value
+//               }
+//             }
+//           : {
+//               ...prev[category],
+//               [field]: value
+//             }
+//       }));
+//       console.log(surveyData);
+// };
 
-const handleNext = () => {
-  const nextStep = step + 1;
-  window.history.pushState({ step: nextStep }, '', `?step=${nextStep}`);
-  setStep(nextStep);
-};
-const handleNext2 = () => {
+// const handleNext = () => {
+//   const nextStep = step + 1;
+//   window.history.pushState({ step: nextStep }, '', `?step=${nextStep}`);
+//   setStep(nextStep);
+// };
+// const handleNext2 = () => {
   
-  setStep(7);
-};
-const handleNext3 = () => {
+//   setStep(7);
+// };
+// const handleNext3 = () => {
   
-  setStep(8);
-};
+//   setStep(8);
+// };
   
-  return (
-    <div className="p-4">
-      {step === 0 && <StartScreen handleNext={handleNext} step={step} setStep={setStep} />}
-      {/* {step === 0 && <StartScreen2 handleNext2={handleNext2} handleNext3={handleNext3}  step={step} setStep={setStep} />} */}
-      {/* {step === 0 && <StartScreen3 handleNext={handleNext} step={step} setStep={setStep} />} */}
-      {step === 1 && <SurveyScreen1 handleNext={handleNext} step={step} setStep={setStep} surveyData = {surveyData} updateAnswer = {updateAnswer}/>}
-      {step === 2 && <SurveyScreen2 handleNext={handleNext} step={step} setStep={setStep} surveyData={surveyData} updateAnswer={updateAnswer}/>}
-      {step === 3 && <SurveyScreen3 handleNext={handleNext} step={step} setStep={setStep} surveyData = {surveyData} updateAnswer = {updateAnswer}/>}
-      {step === 4 && <SurveyScreen4 handleNext={handleNext} step={step} setStep={setStep} surveyData = {surveyData} updateAnswer = {updateAnswer}/>}
-      {step === 5 && <SurveyScreen5 handleNext={handleNext} step={step} setStep={setStep}  surveyData = {surveyData} updateAnswer = {updateAnswer}/>}
-      {step === 6 && <EmailVerificationScreen handleNext={handleNext} step={step} setStep={setStep} surveyData = {surveyData} updateAnswer = {updateAnswer}/>}
-      {step === 7 && <MbtiResult handleNext={handleNext}  step={step}setStep={setStep} surveyData = {surveyData} updateAnswer = {updateAnswer}/>}
-      {step === 8 && <StartPawfectMatch handleNext={handleNext} step={step} setStep={setStep}  surveyData = {surveyData} updateAnswer = {updateAnswer}/>}
-      {step === 9 && <PawfectMatch handleNext={handleNext} step={step} setStep={setStep}  surveyData = {surveyData} updateAnswer = {updateAnswer}/>}
-      {step === 10 && <PawfectMatchResult  handleNext={handleNext} step={step} setStep={setStep} surveyData = {surveyData} updateAnswer = {updateAnswer}/>}
-      {/* {step === 'test' && <TestScreen onComplete={() => {
-        setMbtiResult('ESFP');
-        setStep('result');
-      }} />}
-      {step === 'result' && <ResultScreen mbtiResult={mbtiResult} email={email} setEmail={setEmail} isRegistered={isRegistered} setIsRegistered={setIsRegistered} onNext={() => setStep('pawfectMatch')} />}
-      {step === 'pawfectMatch' && <PawfectMatchScreen />} */}
-    </div>
-  );
-};
+//   return (
+//     <div className="p-4">
+//       {step === 0 && <StartScreen handleNext={handleNext} step={step} setStep={setStep} />}
+//       {/* {step === 0 && <StartScreen2 handleNext2={handleNext2} handleNext3={handleNext3}  step={step} setStep={setStep} />} */}
+//       {/* {step === 0 && <StartScreen3 handleNext={handleNext} step={step} setStep={setStep} />} */}
+//       {step === 1 && <SurveyScreen1 handleNext={handleNext} step={step} setStep={setStep} surveyData = {surveyData} updateAnswer = {updateAnswer}/>}
+//       {step === 2 && <SurveyScreen2 handleNext={handleNext} step={step} setStep={setStep} surveyData={surveyData} updateAnswer={updateAnswer}/>}
+//       {step === 3 && <SurveyScreen3 handleNext={handleNext} step={step} setStep={setStep} surveyData = {surveyData} updateAnswer = {updateAnswer}/>}
+//       {step === 4 && <SurveyScreen4 handleNext={handleNext} step={step} setStep={setStep} surveyData = {surveyData} updateAnswer = {updateAnswer}/>}
+//       {step === 5 && <SurveyScreen5 handleNext={handleNext} step={step} setStep={setStep}  surveyData = {surveyData} updateAnswer = {updateAnswer}/>}
+//       {step === 6 && <EmailVerificationScreen handleNext={handleNext} step={step} setStep={setStep} surveyData = {surveyData} updateAnswer = {updateAnswer}/>}
+//       {step === 7 && <MbtiResult handleNext={handleNext}  step={step}setStep={setStep} surveyData = {surveyData} updateAnswer = {updateAnswer}/>}
+//       {step === 8 && <StartPawfectMatch handleNext={handleNext} step={step} setStep={setStep}  surveyData = {surveyData} updateAnswer = {updateAnswer}/>}
+//       {step === 9 && <PawfectMatch handleNext={handleNext} step={step} setStep={setStep}  surveyData = {surveyData} updateAnswer = {updateAnswer}/>}
+//       {step === 10 && <PawfectMatchResult  handleNext={handleNext} step={step} setStep={setStep} surveyData = {surveyData} updateAnswer = {updateAnswer}/>}
+//       {/* {step === 'test' && <TestScreen onComplete={() => {
+//         setMbtiResult('ESFP');
+//         setStep('result');
+//       }} />}
+//       {step === 'result' && <ResultScreen mbtiResult={mbtiResult} email={email} setEmail={setEmail} isRegistered={isRegistered} setIsRegistered={setIsRegistered} onNext={() => setStep('pawfectMatch')} />}
+//       {step === 'pawfectMatch' && <PawfectMatchScreen />} */}
+//     </div>
+//   );
+// };
 
-export default PetMBTIFlow;
+// export default PetMBTIFlow;
